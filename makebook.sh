b929916bf6b50c5d59ad1cfb1488a3ee8b2c551b
Ben Evans <ben@bensbit.co.uk>
2015-03-25 14:54:35 +0000

    Initial Commit

---
+++ makebook.sh
@@ -0,0 +1,38 @@
+pandoc -S -o coreosdocs.epub docs/README.md \
+  ./docs/running-coreos/cloud-providers/digitalocean/index.md \
+  ./docs/running-coreos/platforms/vagrant/index.md \
+  ./docs/cluster-management/setup/cluster-architectures/index.md \
+  ./docs/cluster-management/setup/update-strategies/index.md \
+  ./docs/cluster-management/setup/cluster-discovery/index.md \
+  ./docs/cluster-management/setup/switching-channels/index.md \
+  ./docs/cluster-management/setup/mounting-storage/index.md \
+  ./docs/cluster-management/setup/network-config-with-networkd/index.md \
+  ./docs/cluster-management/setup/other-settings/index.md \
+  ./docs/cluster-management/setup/flannel-config/index.md \
+  ./docs/cluster-management/debugging/btrfs-troubleshooting/index.md \
+  ./docs/cluster-management/debugging/reading-the-system-log/index.md \
+  ./docs/cluster-management/debugging/install-debugging-tools/index.md \
+  ./docs/launching-containers/building/getting-started-with-docker/index.md \
+  ./docs/launching-containers/building/customizing-docker/index.md \
+  ./docs/launching-containers/building/registry-authentication/index.md \
+  ./docs/launching-containers/launching/launching-containers-fleet/index.md \
+  ./docs/launching-containers/launching/getting-started-with-systemd/index.md \
+  ./docs/launching-containers/launching/overview-of-systemctl/index.md \
+  ./docs/distributed-configuration/getting-started-with-etcd/index.md \
+  ./docs/distributed-configuration/customize-etcd-unit/index.md
+
+
+
+
+# running core os
+#   cloud providers
+#   bare metal
+#   other platforms
+
+# cluster managment
+#   planning a cluster
+#     cluster
+
+# launching containers
+
+# distributed configuration
